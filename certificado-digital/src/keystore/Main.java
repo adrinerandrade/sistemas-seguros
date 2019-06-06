b1fe50f199a38a7b18bdedafa40cc68ec57d7a6e
Adriner Andrade <adriner_andrade@hotmail.com>
2019-06-05 22:01:27 -0300

    questão 1

--- certificado-digital/src/keystore/Main.java
+++ certificado-digital/src/keystore/Main.java
@@ -7,6 +7,7 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateParsingException;
 import java.security.cert.X509Certificate;
 
 public class Main {
@@ -17,8 +18,13 @@ public class Main {
             try {
                 Certificate abstractContract = keyStore.getCertificate(alias);
                 if (abstractContract.getType().equals("X.509")) {
-                       X509Certificate certificate = (X509Certificate) abstractContract;
-
+                    X509Certificate certificate = (X509Certificate) abstractContract;
+                    String issuer = certificate.getIssuerDN().getName();
+                    String subject = certificate.getSubjectDN().getName();
+                    System.out.println(String.format("Emissor: %s", issuer));
+                    System.out.println(String.format("Proprietário: %s", subject));
+                    System.out.println(String.format("Auto-assinado: %s", subject.equals(issuer) ? "Sim" : "Não"));
+                    System.out.println();
                 }
             } catch (KeyStoreException e) {
                 throw new RuntimeException(String.format("Certificado não encontrado: %s", alias));
