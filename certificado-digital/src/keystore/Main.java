2917dd9e4d3b7e128f53ee6ac1555df814469943
Christian Passold <passold.christian@hotmail.com>
2019-06-10 19:06:01 -0300

    Ajuste para o JAVA8 e adicionado imagem encriptada

--- certificado-digital/src/keystore/Main.java
+++ certificado-digital/src/keystore/Main.java
@@ -1,20 +1,28 @@
 package keystore;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
-import java.security.cert.CertificateParsingException;
 import java.security.cert.X509Certificate;
+import java.util.Enumeration;
 
 public class Main {
 
-    public static void main(String[] args) throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
-        KeyStore keyStore = KeyStore.getInstance(new File("./keystore" ), new char[] {'a', 'b', '1','2','3'});
-        keyStore.aliases().asIterator().forEachRemaining(alias -> {
+    public static void main(String[] args) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
+        KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+
+        try (FileInputStream fis = new FileInputStream(new File("./keystore" ))) {
+            keyStore.load(fis, new char[] {'a', 'b', '1','2','3'});
+        }
+
+        final Enumeration<String> aliases = keyStore.aliases();
+        while(aliases.hasMoreElements()){
+            final String alias = aliases.nextElement();
             try {
                 Certificate abstractContract = keyStore.getCertificate(alias);
                 if (abstractContract.getType().equals("X.509")) {
@@ -29,7 +37,7 @@ public class Main {
             } catch (KeyStoreException e) {
                 throw new RuntimeException(String.format("Certificado não encontrado: %s", alias));
             }
-        });
+        }
     }
 
 }
