951e40f3993486a2ce46ad8f76646af722f3c43a
Christian Passold <passold.christian@hotmail.com>
2019-06-10 19:49:19 -0300

    Criado a chave encriptada

--- certificado-digital/src/keystore/Main.java
+++ certificado-digital/src/keystore/Main.java
@@ -1,25 +1,33 @@
 package keystore;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.*;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.Enumeration;
+import java.util.HashMap;
 
 public class Main {
 
-    public static void main(String[] args) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
+    public static void main(String[] args) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchPaddingException {
         KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
 
         try (FileInputStream fis = new FileInputStream(new File("./keystore"))) {
             keyStore.load(fis, new char[]{'a', 'b', '1', '2', '3'});
         }
 
+        HashMap<Integer, PublicKey> publicKeyHashMap = new HashMap<>();
+        int count = 1;
         final Enumeration<String> aliases = keyStore.aliases();
         while (aliases.hasMoreElements()) {
             final String alias = aliases.nextElement();
@@ -33,11 +41,32 @@ public class Main {
                     System.out.println(String.format("Proprietário: %s", subject));
                     System.out.println(String.format("Auto-assinado: %s", subject.equals(issuer) ? "Sim" : "Não"));
                     System.out.println();
+
+                    publicKeyHashMap.put(count, certificate.getPublicKey());
+                    count++;
                 }
             } catch (KeyStoreException e) {
                 throw new RuntimeException(String.format("Certificado não encontrado: %s", alias));
             }
         }
+
+        writeFile(new File("key.aes"), Files.readAllBytes(Paths.get("key.txt")), publicKeyHashMap.get(1));
+    }
+
+    private static void writeFile(File output, byte[] bytes, PublicKey publicKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException, BadPaddingException, IllegalBlockSizeException {
+        final Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+        if(output.exists()){
+            output.delete();
+        }
+
+        FileOutputStream fos = new FileOutputStream(output);
+        fos.write(cipher.doFinal(bytes));
+        fos.flush();
+        fos.close();
+
+        System.out.println("Chave encriptada:" + output.toString());
     }
 
 }
